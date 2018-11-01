1d408d839c89c704e259600ab0df3e983922c188
krutika <krr39@njit.edu>
2018-11-01 01:34:56 -0400

    5 commit Car factory created ford honda toyota

--- database/factories/CarFactory.php
+++ database/factories/CarFactory.php
@@ -4,6 +4,8 @@ use Faker\Generator as Faker;
 
 $factory->define(Model::class, function (Faker $faker) {
     return [
-        //
+        'Make' => $faker->randomElement($array = array ('honda','toyota','ford')),
+        'Model' => $faker->str_random(10),
+        'Year'=> $faker->year($max='now'),
     ];
 });
