ceb9d8bd9a7e94725b28480e1fc4b9bb121d6006
krutika <krr39@njit.edu>
2018-11-02 01:28:00 -0400

    Unit test 7 delete record from table

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -10,7 +10,7 @@ class ExampleTest extends TestCase
     /**
      * A basic test example.
      *  public function testInsertUserTable()
-   
+
      * @return void
      */
     public function testBasicTest()
--- tests/Unit/UnitTest.php
+++ tests/Unit/UnitTest.php
@@ -20,11 +20,22 @@ class UnitTest extends TestCase
         $user->password = 'pasjjs1233';
         $this->assertTrue($user->save());
     }
+    //unit test 2
     public function testUpdateSteveNameTable()
     {
         $user = User::find(1);
         $user->name = 'Steve Smith';
         $this->assertTrue($user->save());
+    }
+    //unit test 3
+    public function testDeleteUserTable()
+    {
+        $user= new User();
+        $user->name = 'Prof. Estban Lowe';
+        $user->email = 'derek95@exaple.org';
+        $user->password = '123456';
+        $user->save();
+        $this->assertTrue($user->delete());
     }
 
 }
