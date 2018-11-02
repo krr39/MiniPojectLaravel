e3ff2a105d8683241a4149eb2a30e5dd085d5d48
krutika <krr39@njit.edu>
2018-11-02 00:16:50 -0400

    7 commit Contact page created

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -33,6 +33,13 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav mr-auto">
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('contact') }}">{{ __('Contact') }}</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('about') }}">{{ __('About') }}</a>
+                        </li>
+
 
                     </ul>
 
